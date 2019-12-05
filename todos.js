9a834c85dde876718990f36df52b2f0f8af608d5
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-12-05 14:04:22 -0500

    Add todo creation ability and update data modeling
    We now update the todo list when we take an action and re-render and attach events accordingly

--- todos.js
+++ todos.js
@@ -42,12 +42,53 @@ const resetCheckboxStatus = el => {
 /**
  * removeTodo
  *
- * Remove a todo from the DOM
+ * Remove a todo from the DOM and data model
  *
+ * @param {array} todos
+ * @param {number} id
  * @param {DOM Element} el
  * @return {void}
  */
-const removeTodo = el => el.remove();
+const removeTodo = (todos, id, el) => {
+  const todoIndex = todos.findIndex(todo => todo.id === id);
+
+  todos.splice(todoIndex, 1);
+
+  el.remove()
+};
+
+/**
+ * addTodo
+ *
+ * Add new todo to the todos data model
+ *
+ * @param {array} todos
+ * @param {number} userId
+ * @param {title} title
+ * @param {boolean} completed
+ * @return {boid}
+ */
+const addTodo = (todos, userId, title, completed) => {
+  // KLUDGE: Normally we would use the returned id from the POST response, but
+  // the API returns the same stubbed response every time, so we need to create
+  // new ids
+  const id = Math.floor(Math.random() * Math.floor(100000));
+
+  // Add new todo to the front of the list
+  todos.unshift({ userId, id, title, completed });
+};
+
+/**
+ * updateTodoStatus
+ *
+ * Modify the status of the todo in the data model
+ *
+ * @param {array} todos
+ * @param {number} id
+ * @param {boolean} status
+ * @return {void}
+ */
+const updateTodoStatus = (todos, id, status) => todos.find(todo => todo.id === id).completed = status;
 
 /**
  * addTodoClickEventHandlers
@@ -56,22 +97,26 @@ const removeTodo = el => el.remove();
  *
  * @param {number} id
  * @param {boolean} status
+ * @param {array} todos
  * @return {void}
  */
-const addTodoClickEventHandlers = (id, status) => {
+const addTodoClickEventHandlers = (id, status, todos) => {
   const todoEl = window.document.getElementById(`todo-${id}`);
-  let todoCheckboxEl = todoEl.getElementsByTagName('input')[0];
+  const todoCheckboxEl = todoEl.getElementsByTagName('input')[0];
+  const newStatus = !status;
+
   todoCheckboxEl.addEventListener('click', e => {
     displayLoading();
 
     fetch(`${TODOS_URL}/${id}`, {
       method: 'PATCH',
-      body: JSON.stringify({ completed: !status }),
+      body: JSON.stringify({ completed: newStatus }),
       headers: { "Content-type": "application/json; charset=UTF-8" }
     })
       .then(res => res.json())
       .then(json => {
-        handleCompletedUiStatusForTodo(json.id);
+        updateTodoStatus(todos, id, newStatus);
+        handleCompletedUiStatusForTodo(id);
         removeLoading();
       })
       .catch(err => {
@@ -81,14 +126,14 @@ const addTodoClickEventHandlers = (id, status) => {
       });
   });
 
-  let deleteButtonEl = window.document.getElementById(`todo-${id}`).querySelector('.delete-btn');
+  const deleteButtonEl = window.document.getElementById(`todo-${id}`).querySelector('.delete-btn');
   deleteButtonEl.addEventListener('click', e => {
     if (window.confirm('Are you sure you wish to delete this todo item?')) {
       displayLoading();
 
       fetch(`${TODOS_URL}/${id}`, { method: 'DELETE' })
         .then(res => {
-          removeTodo(todoEl);
+          removeTodo(todos, id, todoEl);
           removeLoading();
         })
         .catch(err => {
@@ -126,6 +171,44 @@ const getNewTodo = (id, title, completed) => {
 };
 
 /**
+ * addEventHandlerForCreatingTodos
+ *
+ * Add the event handles necessary to allow creation of new todo items
+ *
+ * @param {number} userId
+ * @param {array} todos
+ * @return {void}
+ */
+const addEventHandlerForCreatingTodos = (userId, todos) => {
+  const addButtonEl = window.document.getElementById('todo-add-container').querySelector('.add-btn');
+  const inputTodoEl = window.document.getElementById('todo-add');
+
+  addButtonEl.addEventListener('click', e => {
+    displayLoading();
+
+    fetch(`${TODOS_URL}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: inputTodoEl.value,
+        completed: false,
+        userId
+      }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    })
+      .then(res => res.json())
+      .then(todo => {
+        addTodo(todos, userId, todo.title, todo.completed)
+        displayTodos(todos);
+        removeLoading();
+      })
+      .catch(err => {
+        displayError(err);
+        removeLoading();
+      });
+  });
+};
+
+/**
  * displayTodos
  *
  * Handles display of todo lists
@@ -148,5 +231,6 @@ const displayTodos = todos => {
   todoList += '</ul>';
   todosContainerEl.innerHTML = `${todoContainerHeader}${todoAdder}${todoList}`;
 
-  todos.forEach(todo => addTodoClickEventHandlers(todo.id, todo.completed));
+  todos.forEach(todo => addTodoClickEventHandlers(todo.id, todo.completed, todos));
+  addEventHandlerForCreatingTodos(todos[0].userId, todos);
 };
