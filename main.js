f1a5e5858a7a73e7f702f55f3e257c809fb05d37
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-12-04 22:03:55 -0500

    Simplify code with fetch API

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-const fetch = url => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send(null);
-
-  return xhr;
-};
-
-const getXhrResponseData = xhr => {
-  const parseData = data => JSON.parse(data);
-
-  xhr.onreadystatechange = () => {
-    const DONE = 4;
-    const OK = 200;
-
-    if (xhr.readyState === DONE) {
-      if (xhr.status === OK) {
-        return { hasError: false, isFinished: true, data: parseData(xhr.responseText) };
-      } else {
-        return { hasError: true, isFinished: true, data: xhr.status };
-      }
-    } else {
-      return { hasError: undefined, isFinished: false, data: undefined };
-    }
-  };
-
-  return xhr.onreadystatechange();
-};
-
 const displayUsers = users => {
   const usersEl = window.document.querySelector('#users-container');
 
@@ -35,25 +6,19 @@ const displayUsers = users => {
   usersEl.innerHTML += '</ul>';
 };
 
+const displayError = msg => {
+  const errorsEl = window.document.querySelector('#error-container');
+
+  errorsEl.innerHTML = `<p>${msg}</p>`;
+};
+
 const start = () => {
   const usersEl = window.document.querySelector('#users-container');
-  const usersXhr = fetch('https://jsonplaceholder.typicode.com/users');
-  const interval = window.setInterval(
-    () => {
-      const users = getXhrResponseData(usersXhr);
-
-      if (users.isFinished) {
-        window.clearInterval(interval);
-
-        if (users.hasError === false) displayUsers(users.data);
-        else usersEl.innerHTML = '<p>Error!</p>';
-      }
-    },
-    1100,
-    usersXhr
-  );
-
   usersEl.innerHTML = '<p>Loading...</p>';
+  users = fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => displayUsers(json))
+    .catch(err => displayError(err));
 };
 
 start();
