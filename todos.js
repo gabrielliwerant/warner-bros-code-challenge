055df96d7278f8d842e007176f818b91bfaa2417
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-12-05 03:01:04 -0500

    Add ability to check/uncheck todos for completedness
    In support of this feature, add a loading overlay

--- todos.js
+++ todos.js
@@ -1,4 +1,76 @@
 /**
+ * handleCompletedUiStatusForTodo
+ *
+ * Handles the toggling of the completed status for the todo list item for
+ * display purposes
+ *
+ * @param {number} id
+ * @return {void}
+ */
+const handleCompletedUiStatusForTodo = id => {
+  const todoEl = window.document.getElementById(`todo-${id}`);
+
+  // Find completed status from class name
+  const isCompleted = !todoEl.getAttribute('class').includes('uncompleted');
+
+  // Toggle the appropriate completed classes
+  if (isCompleted) {
+    todoEl.classList.remove('completed');
+    todoEl.classList.add('uncompleted');
+  } else {
+    todoEl.classList.remove('uncompleted');
+    todoEl.classList.add('completed');
+  }
+};
+
+/**
+ * resetCheckboxStatus
+ *
+ * In the event that we need the original checkbox completed status (such as in
+ * the event of a response error), we reset it here
+ *
+ * @param {DOM Element} el
+ * @returns {void}
+ */
+const resetCheckboxStatus = el => {
+  if (el.checked) el.checked = false;
+  else el.checked = true;
+};
+
+/**
+ * addTodoClickEventHandlers
+ *
+ * Adds event handlers related to clicking on a todo todo checkbox
+ *
+ * @param {number} id
+ * @param {boolean} status
+ * @return {void}
+ */
+const addTodoClickEventHandlers = (id, status) => {
+  let todoCheckboxEl = window.document.getElementById(`todo-${id}`).getElementsByTagName('input')[0];
+
+  todoCheckboxEl.addEventListener('click', e => {
+    let loadingEl = window.document.querySelector('#loading-overlay');
+    displayLoading(loadingEl);
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ completed: !status }),
+      headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+    .then(res => res.json())
+    .then(json => {
+      handleCompletedUiStatusForTodo(json.id);
+      loadingEl.innerHTML = '';
+    })
+    .catch(err => {
+      displayError(err);
+      resetCheckboxStatus(todoCheckboxEl);
+    });
+  });
+};
+
+/**
  * displayTodos
  *
  * Handles display of todo lists
@@ -25,4 +97,6 @@ const displayTodos = todos => {
   });
   todoList += '</ul>';
   todosContainerEl.innerHTML = todoList;
+
+  todos.forEach(todo => addTodoClickEventHandlers(todo.id, todo.completed));
 };
